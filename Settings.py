50cfc37370eae39e9f9db4738746fbd88c7b2368
M Zhou <contact@mzhou.me>
2015-10-19 22:32:50 +0800

    Functional well

--- Settings.py
+++ Settings.py
@@ -3,21 +3,29 @@ import sublime_plugin
 
 
 class Settings:
-    settings_base = 'Task Manager.sublime-settings'
-    settings = {}
+    settings_base = 'TaskManager.sublime-settings'
 
-    @staticmethod
-    def load():
-        Settings.settings = sublime.load_settings(Settings.settings_base)
+    def __init__(self):
+        self.settings = sublime.load_settings(Settings.settings_base)
 
-    @staticmethod
-    def get_global(key, default=None):
-        return Settings.settings.get(key, default)
-
-    @staticmethod
-    def get_project(key, default=None):
+    def get_tasks(self):
+        tasks = self.settings.get('builtin-tasks')
+        user_tasks = self.settings.get('user-tasks', None)
+        if user_tasks:
+            tasks.extend(user_tasks)
         window = sublime.active_window()
         project_data = window.project_data()
-        if (project_data is not None and
-                'tasks' in project_data):
-            return project_data['tasks'].get(key, default)
+        if project_data and 'proj-tasks' in project_data:
+            tasks.extend(project_data['proj-tasks'])
+        return tasks
+
+    def get_global(self, key, default=None):
+        return self.settings.get(key, default)
+
+    def get_project(self, key, default=None):
+        window = sublime.active_window()
+        project_data = window.project_data()
+        if (project_data is not None):
+            return project_data.get(key, default)
+        else:
+            return default
--- TaskManager.py
+++ TaskManager.py
@@ -1,1 +1,60 @@
-print('Task Manager is loading...')
+import os
+import sys
+import subprocess
+import sublime
+import sublime_plugin
+from .Settings import Settings
+
+
+class RunTasksCommand(sublime_plugin.WindowCommand):
+
+    def __init__(self, window):
+        self.settings = Settings()
+        self.window = window
+
+    def run(self, *args):
+        self.tasks = self.settings.get_tasks()
+        task_names = [t.get('caption') for t in self.tasks]
+        self.window.show_quick_panel(task_names, self.on_selected)
+
+    def on_selected(self, index):
+        if index == -1:
+            return
+
+        task = self.tasks[index]
+        if task['sublime-command']:
+            self.window.run_command(task['command'], task['args'])
+        else:
+            folders = self.window.folders()
+            if len(folders) > 1:
+                def folders_selected(ind):
+                    nonlocal cwd
+                    cwd = folders[ind]
+                self.window.show_quick_panel(folders, folders_selected)
+            elif len(folders) == 1:
+                cwd = folders[0]
+            else:
+                cwd = os.path.expanduser('~')
+            print([task['command'], task['args']])
+            process = subprocess.Popen([task['command'], task['args']],
+                                       shell=False,
+                                       cwd=cwd,
+                                       stdin=subprocess.PIPE,
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+            out, err = process.communicate()
+            encoding = self.settings.get_global('encoding', 'utf-8')
+            out = out.decode(encoding)
+            err = err.decode(encoding)
+            if sublime.platform() == 'windows':
+                out = out.replace('\r\n', '\n')
+                err = err.replace('\r\n', '\n')
+            content = out if err == '' else out + '\n\n>>> ERROR:\n' + err
+            panel = self.window.create_output_panel('task_output')
+            panel.settings().set('scroll_past_end', False)
+            panel.settings().set('auto_indent', False)
+            panel.set_read_only(False)
+            panel.run_command('insert', {"characters": content})
+            panel.set_read_only(True)
+            self.window.run_command("show_panel",
+                                    {"panel": "output.task_output"})
